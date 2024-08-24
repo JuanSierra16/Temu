0e9fa50f9524b27a689921e156f87866732738fb
juliankrdnas <juliankrdnas@gmail.com>
2024-08-24 14:27:40 -0500

    Agregar NavBar, Agregar BlackBar, Modificar App

---
+++ frontend-temu/src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+// src/components/MainContent.jsx
+import React from 'react';
+import './MainContent.css'
+
+const MainContent = () => {
+    return (
+        <main>
+            <h2>Bienvenido a Temu</h2>
+            {/* Contenido principal aquí */}
+        </main>
+    );
+};
+
+export default MainContent;
