6a5e87bd535e55a78b136d31aaa62cef92dcec10
Juan Jose <juanjose.sierra@utp.edu.co>
2024-08-25 18:29:26 -0500

    Añadir CORS

--- backend-temu/src/index.js
+++ backend-temu/src/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import usersRoutes from './routes/users.routes.js';
+import cors from 'cors';
 
 const app = express();
 
+// Configurar CORS con opciones avanzadas
+const corsOptions = {
+    origin: 'http://localhost:5173', // URL de tu frontend
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(usersRoutes);
