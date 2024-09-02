b00869f9efc4de1f40b1ac41bc93e959ee1cad3b
Juan <juandiego.velez@utp.edu.co>
2024-09-01 23:51:48 -0500

    Mejorar el la barra de información

--- frontend-temu/src/components/BlackBar.jsx
+++ frontend-temu/src/components/BlackBar.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import './BlackBar.css'; // Asegúrate de crear y aplicar este archivo CSS
 import { FaTruckFast } from 'react-icons/fa6';
 import { MdOutlineAssignmentReturn } from 'react-icons/md';
 import { IoIosPhonePortrait } from 'react-icons/io';
 
+import './BlackBar.css';
+
 const BlackBar = () => {
     return (
-        <div className="BlackBar">
-            <div className="BlackBar__item">
-                <span>Envío gratis en cada pedido</span>
-                <small>Oferta exclusiva</small>
-                <FaTruckFast />
-            </div>
-            <div className="BlackBar__item">
-                <span>Devoluciones: 90 días</span>
-                <small>desde la fecha de compra</small>
-                <MdOutlineAssignmentReturn />
-            </div>
-            <div className="BlackBar__item">
+        <section className="bar flex-row space-around">
+            <article className="flex-row">
+                <FaTruckFast size={32} />
+
+                <div className="flex-col align-start">
+                    <span>Envío gratis en cada pedido</span>
+                    <small>Oferta exclusiva</small>
+                </div>
+            </article>
+
+            <div className="vertical-line"></div>
+
+            <article className="flex-row">
+                <MdOutlineAssignmentReturn size={32} />
+
+                <div className="flex-col align-start">
+                    <span>Devoluciones: 90 días</span>
+                    <small>desde la fecha de compra</small>
+                </div>
+            </article>
+
+            <div className="vertical-line"></div>
+
+            <article className="flex-row">
+                <IoIosPhonePortrait size={32} />
                 <span>Descarga la app de Temu</span>
-                {/* Probando 123 */}
-                <IoIosPhonePortrait />
-            </div>
-        </div>
+            </article>
+        </section>
     );
 };
 
