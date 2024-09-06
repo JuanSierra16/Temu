753baeff28f09d4bf2363cc2c3014a62a6d338c7
Juan <juandiego.velez@utp.edu.co>
2024-09-06 00:16:33 -0500

    Guardar session de usuario y token en las cookies usando js-cookie

--- frontend-temu/src/provider/UserContext.jsx
+++ frontend-temu/src/provider/UserContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from 'react';
 import { login } from '../API/Login.API';
+import Cookies from 'js-cookie';
 
 const UserContext = createContext(null);
 
@@ -13,6 +14,19 @@ const UserProvider = ({ children }) => {
     const [loginError, setLoginError] = useState(null);
     const [userIsLogin, setUserIsLogin] = useState(false);
     const [waitLogin, setWaitLogin] = useState(false);
+    const [sessionJWTToken, setSessionJWTToken] = useState(null);
+
+    useEffect(() => {
+        const cookieValue = Cookies.get('token');
+
+        if (cookieValue) {
+            const { user, token } = JSON.parse(cookieValue);
+
+            setUserData(user);
+            setUserIsLogin(true);
+            setSessionJWTToken(token);
+        }
+    }, []);
 
     const loginAction = async (email, password) => {
         setWaitLogin(true);
@@ -21,10 +35,24 @@ const UserProvider = ({ children }) => {
             const data = await login(email, password);
             setUserData(data.user);
             setUserIsLogin(true);
+            setSessionJWTToken(data.token);
+
+            Cookies.set(
+                'token',
+                JSON.stringify({ user: data.user, token: data.token }),
+                { expires: 1 },
+            );
         } catch (error) {
-            setUserData(null);
-            setUserIsLogin(false);
+            console.log(error);
             setLoginError('Credenciales incorrectas');
+
+            setUserData({
+                id: '',
+                username: '',
+                email: '',
+            });
+
+            setUserIsLogin(false);
 
             setTimeout(() => {
                 setLoginError(null);
@@ -34,11 +62,24 @@ const UserProvider = ({ children }) => {
         setWaitLogin(false);
     };
 
+    const logoutAction = () => {
+        Cookies.remove('token');
+
+        setUserData({
+            id: '',
+            username: '',
+            email: '',
+        });
+
+        setUserIsLogin(false);
+    };
+
     return (
         <UserContext.Provider
             value={{
                 userData,
                 loginAction,
+                logoutAction,
                 loginError,
                 userIsLogin,
                 waitLogin,
