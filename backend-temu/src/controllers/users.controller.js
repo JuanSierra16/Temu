803c5f48540d8f6dc1e2ec1ea9ba6ebd620642cf
Juan Jose <juanjose.sierra@utp.edu.co>
2024-09-07 12:18:21 -0500

    Agregar enpoint para login con plataforma

--- backend-temu/src/controllers/users.controller.js
+++ backend-temu/src/controllers/users.controller.js
@@ -59,3 +59,46 @@ export const loginUser = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 }
+
+export const loginUserPlatform = async (req, res) => {
+    const { id_usuario_plataforma, nombre_plataforma, username, email } = req.body;
+
+    try {
+        // Buscar si el usuario ya existe en la base de datos
+        const [rows] = await pool.query('SELECT * FROM users WHERE id_usuario_plataforma = ?', [id_usuario_plataforma]);
+
+        let user;
+        if (rows.length === 0) {
+            // Si el usuario no existe, crear un nuevo usuario
+            const [insertResult] = await pool.query('INSERT INTO users (id_usuario_plataforma, nombre_plataforma, username, email) VALUES (?, ?, ?, ?)', [id_usuario_plataforma, nombre_plataforma, username, email]);
+            user = {
+                id: insertResult.insertId,
+                id_usuario_plataforma,
+                nombre_plataforma,
+                username,
+                email
+            };
+        } else {
+            // Si el usuario ya existe, obtener sus datos
+            user = rows[0];
+        }
+
+        // Generar el token JWT
+        const token = jwt.sign({ id: user.id, username: user.username, email: user.email }, SECRET_KEY, { expiresIn: '1h' });
+
+        // Devolver el ID del usuario y el token al front-end
+        res.send({
+            message: 'Inicio de sesión exitoso',
+            user: {
+                id: user.id,
+                id_usuario_plataforma: user.id_usuario_plataforma,
+                nombre_plataforma: user.nombre_plataforma,
+                username: user.username,
+                email: user.email,
+            },
+            token: token, // Devolver el token al front-end
+        });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
