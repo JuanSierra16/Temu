43c4f85096af40502c9a913ad0f39136358e3b5d
Juan <juandiego.velez@utp.edu.co>
2024-09-22 13:20:04 -0500

    Buscar cuenta por email o número de teléfono

--- frontend-temu/src/pages/FindAccount.jsx
+++ frontend-temu/src/pages/FindAccount.jsx
@@ -1,56 +1,229 @@
-import { FaChevronRight } from 'react-icons/fa';
+import './FindAccount.css';
+import { FaUserCircle, FaChevronRight } from 'react-icons/fa';
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../provider/UserContext';
 import NavBar from '../components/sections/navbar/NavBar';
 import Footer from '../components/sections/Footer';
-
-import './FindAccount.css';
+import Modal from '../components/elements/Modal';
+import Select from '../components/elements/Select';
+import { supportPrefixPhone } from '../utils/prefixPhone';
 
 const FindAccount = () => {
+    const [phonePrefix, setPhonePrefix] = useState('');
+    const [countryValue, setCountryValue] = useState('');
+
+    const [findByEmailModal, setFindByEmailModal] = useState(false);
+    const [findByPhoneModal, setFindByPhoneModal] = useState(false);
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [success, setSuccess] = useState(false);
+    const [findData, setFindData] = useState({
+        username: null,
+        email: null,
+        phone_number: null,
+        message: null,
+    });
+
+    const { findAccountWithEmail, findAccountWithPhoneNumber, loginError } =
+        useContext(UserContext);
+
+    const findAccountEmail = () => {
+        findAccountWithEmail(email).then(data => {
+            if (data.username) {
+                setFindData(data);
+                setFindByEmailModal(false);
+                setSuccess(true);
+            }
+        });
+    };
+
+    const findAccountPhone = () => {
+        findAccountWithPhoneNumber(`${phonePrefix}${phone}`).then(data => {
+            if (data.username) {
+                setFindData(data);
+                setFindByPhoneModal(false);
+                setSuccess(true);
+            }
+        });
+    };
+
+    useEffect(() => {
+        // por defecto el primer prefijo de la lista
+        const { countryCode, prefix } = supportPrefixPhone[0];
+        setCountryValue(`${countryCode} ${prefix}`);
+        setPhonePrefix(prefix);
+    }, []);
+
+    const handleSelectChange = e => {
+        // Obtener el prefijo de número de teléfono seleccionado de la lista
+        const { countryCode, prefix } = supportPrefixPhone.find(
+            item => item.country == e.target.getAttribute('value'),
+        );
+
+        setCountryValue(`${countryCode} ${prefix}`);
+        setPhonePrefix(prefix);
+    };
+
     return (
         <main>
             <NavBar />
 
             <article className="max-width find-account">
-                <h2>Elige una forma de encontrar tu cuenta</h2>
+                {!success && (
+                    <>
+                        <h2>Elige una forma de encontrar tu cuenta</h2>
 
-                <p>
-                    Puedes elegir una de las siguientes maneras de encontrar tu
-                    cuenta:
-                </p>
+                        <p>
+                            Puedes elegir una de las siguientes maneras de
+                            encontrar tu cuenta:
+                        </p>
 
-                <div className="find-account-grid">
-                    <div className="find-account-item">
-                        <div>
-                            <p>
-                                Proporciona una dirección de email para
-                                encontrar la cuenta
-                            </p>
+                        <div className="find-account-grid">
+                            <div
+                                className="find-account-item"
+                                onClick={() => setFindByEmailModal(true)}
+                            >
+                                <div>
+                                    <p>
+                                        Proporciona una dirección de email para
+                                        encontrar la cuenta
+                                    </p>
 
-                            <small>
-                                Puede encontrar una cuenta que coincida de
-                                manera similar por email
-                            </small>
+                                    <small>
+                                        Puede encontrar una cuenta que coincida
+                                        de manera similar por email
+                                    </small>
+                                </div>
+
+                                <FaChevronRight />
+                            </div>
+
+                            <div
+                                className="find-account-item"
+                                onClick={() => setFindByPhoneModal(true)}
+                            >
+                                <p>
+                                    Proporciona un número de teléfono para
+                                    encontrar la cuenta
+                                </p>
+                                <FaChevronRight />
+                            </div>
+
+                            <div className="find-account-item">
+                                <p>
+                                    Proporciona el ID del pedido para encontrar
+                                    la cuenta
+                                </p>
+                                <FaChevronRight />
+                            </div>
                         </div>
+                    </>
+                )}
 
-                        <FaChevronRight />
-                    </div>
+                {success && (
+                    <>
+                        <h2>
+                            Hemos encontrado una cuenta que coincide con lo que
+                            has introducido.
+                        </h2>
 
-                    <div className="find-account-item">
-                        <p>
-                            Proporciona un número de teléfono para encontrar la
-                            cuenta
-                        </p>
-                        <FaChevronRight />
-                    </div>
+                        <div className="find-account-item">
+                            <div className="find-account-user">
+                                <FaUserCircle size={48} />
 
-                    <div className="find-account-item">
-                        <p>
-                            Proporciona el ID del pedido para encontrar la
-                            cuenta
-                        </p>
-                        <FaChevronRight />
-                    </div>
-                </div>
+                                <div>
+                                    <p>{findData.username}</p>
+                                    <small>{findData.email}</small>
+                                    {findData.phone_number && (
+                                        <small>{findData.phone_number}</small>
+                                    )}
+                                </div>
+                            </div>
+
+                            <FaChevronRight />
+                        </div>
+                    </>
+                )}
             </article>
+
+            <Modal show={findByEmailModal} setShow={setFindByEmailModal}>
+                <div className="find-account-modal">
+                    <h5>
+                        Proporciona una dirección de email para buscar el pedido
+                    </h5>
+
+                    <p>
+                        Ingresa tu email para encontrar una cuenta que coincida
+                        total o parcialmente con lo que ingresaste.
+                    </p>
+
+                    <label htmlFor="email">
+                        Dirección de email
+                        <input
+                            type="email"
+                            name="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </label>
+
+                    <button
+                        className="orange-button"
+                        onClick={findAccountEmail}
+                    >
+                        Encontrar cuenta
+                    </button>
+
+                    {findData.message && <small>{findData.message}</small>}
+                    {loginError && <small>{loginError}</small>}
+                </div>
+            </Modal>
+
+            <Modal show={findByPhoneModal} setShow={setFindByPhoneModal}>
+                <div className="find-account-modal">
+                    <h5>
+                        Proporciona un número de teléfono para buscar la cuenta
+                    </h5>
+
+                    <p>
+                        Ingresa el número de teléfono asociado con tu cuenta
+                        Temu para encontrar tu cuenta.
+                    </p>
+
+                    <label htmlFor="phone">Número de teléfono</label>
+
+                    <div className="phone-input">
+                        <Select value={countryValue}>
+                            {supportPrefixPhone.map(({ country, prefix }) => (
+                                <p
+                                    className="phone-select"
+                                    key={country}
+                                    value={country}
+                                    onClick={handleSelectChange}
+                                >
+                                    {country} {prefix}
+                                </p>
+                            ))}
+                        </Select>
+                        <input
+                            type="tel"
+                            name="phone"
+                            value={phone}
+                            onChange={e => setPhone(e.target.value)}
+                        />
+                    </div>
+
+                    <button
+                        className="orange-button"
+                        onClick={findAccountPhone}
+                    >
+                        Encontrar cuenta
+                    </button>
+
+                    {findData.message && <small>{findData.message}</small>}
+                    {loginError && <small>{loginError}</small>}
+                </div>
+            </Modal>
 
             <Footer />
         </main>
