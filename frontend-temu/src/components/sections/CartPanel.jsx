c66fdf4241ffd1882f0708fd75cc5b5ee2f31aff
Juan <juandiego.velez@utp.edu.co>
2024-10-10 00:25:43 -0500

    Crear panel lateral del carrito de compra

---
+++ frontend-temu/src/components/sections/CartPanel.jsx
@@ -0,0 +1,46 @@
+import { useContext } from 'react';
+import './CartPanel.css';
+import { BsCartCheck } from 'react-icons/bs';
+import { CartContext } from '../../provider/CartContext';
+import { Link, useNavigate } from 'react-router-dom';
+
+const CartPanel = () => {
+    const { cart, cartTotalCost } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    return (
+        <div
+            className={`cart-panel ${cart.length === 0 ? 'cart-panel-hidden' : ''}`}
+        >
+            <div className="cart-panel-title">
+                <BsCartCheck size={32} />
+                Subtotal
+            </div>
+
+            <p>
+                <strong>${cartTotalCost}</strong>
+            </p>
+
+            <button className="orange-button">
+                Hacer {cart.length <= 1 ? 'pedido' : 'pedidos'} ({cart.length})
+            </button>
+
+            <button onClick={() => navigate('/cart')}>Ir al carrito</button>
+
+            <div className="cart-panel-products">
+                {cart.map(product => (
+                    <Link
+                        className="cart-panel-product"
+                        key={product.title}
+                        to={`/product/${encodeURIComponent(product.title)}`}
+                    >
+                        <img src={`/products/${product.img}`} alt="" />
+                        <p className="orange-text">${product.price}</p>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default CartPanel;
