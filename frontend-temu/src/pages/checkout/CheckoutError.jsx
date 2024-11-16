f31128ef0e5c1c2a0d4fef0a8f07f247408eca9d
Juan <juandiego.velez@utp.edu.co>
2024-11-16 12:03:15 -0500

    Limpiar carrito de compras después de realizar un pago exitoso

--- frontend-temu/src/pages/checkout/CheckoutError.jsx
+++ frontend-temu/src/pages/checkout/CheckoutError.jsx
@@ -1,6 +1,26 @@
+import { useEffect } from 'react';
 import './Checkout.css';
 
 const CheckoutError = () => {
+    useEffect(() => {
+        const tokenFromUrl = new URLSearchParams(window.location.search).get(
+            'code',
+        );
+        const checkoutState = localStorage.getItem('checkout-state') || null;
+        const code = localStorage.getItem('checkout-code') || null;
+
+        if (
+            tokenFromUrl &&
+            code &&
+            tokenFromUrl === code &&
+            checkoutState !== 'success' &&
+            checkoutState === 'processing'
+        ) {
+            localStorage.setItem('checkout-state', 'cancel');
+            localStorage.removeItem('checkout-code');
+        }
+    }, []);
+
     return (
         <div className="checkout-error-container">
             <h1>Hubo un error con tu compra</h1>
--- frontend-temu/src/pages/checkout/CheckoutSuccess.jsx
+++ frontend-temu/src/pages/checkout/CheckoutSuccess.jsx
@@ -1,19 +1,42 @@
 import './Checkout.css';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const CheckoutSuccess = () => {
+    const navigation = useNavigate();
+
+    useEffect(() => {
+        const tokenFromUrl = new URLSearchParams(window.location.search).get(
+            'code',
+        );
+        const checkoutState = localStorage.getItem('checkout-state') || null;
+        const code = localStorage.getItem('checkout-code') || null;
+
+        if (
+            tokenFromUrl &&
+            code &&
+            tokenFromUrl === code &&
+            checkoutState !== 'cancel' &&
+            checkoutState === 'processing'
+        ) {
+            localStorage.setItem('checkout-state', 'success');
+            localStorage.removeItem('checkout-code');
+        }
+    }, []);
+
     return (
-        <>
-            <main className="checkout-success-container max-width">
-                <h1>¡Gracias por tu compra!</h1>
+        <main className="checkout-success-container max-width">
+            <h1>¡Gracias por tu compra!</h1>
 
-                <p>
-                    Tu compra ha sido realizada con éxito. En breve recibirás un
-                    correo con la información de tu compra.
-                </p>
+            <p>
+                Tu compra ha sido realizada con éxito. En breve recibirás un
+                correo con la información de tu compra.
+            </p>
 
-                <img src="/checkout-success.png" alt="" />
-            </main>
-        </>
+            <img src="/checkout-success.png" alt="" />
+
+            <button onClick={() => navigation('/')}>Volver al inicio</button>
+        </main>
     );
 };
 
