6a8c6a2836241da315004ef095862f6f3428937d
Juan <juandiego.velez@utp.edu.co>
2024-11-17 00:29:56 -0500

    Mostrar detalles de pedido incluido detalles de productos

--- frontend-temu/src/pages/user/YourOrders.jsx
+++ frontend-temu/src/pages/user/YourOrders.jsx
@@ -6,6 +6,9 @@ import { fetchOrders } from '../../API/Orders.API';
 import { UserContext } from '../../provider/UserContext';
 import AddressComponent from '../checkout/AddressComponent';
 import { useCountry } from '../../provider/UseCountry';
+import Modal from '../../components/elements/Modal';
+import { getProductById } from '../../API/Products.API';
+import Slider from '../../components/elements/Slider';
 
 const YourOrders = () => {
     const orderTypes = [
@@ -20,6 +23,9 @@ const YourOrders = () => {
     const [orders, setOrders] = useState([]);
     const { userData } = useContext(UserContext);
     const { formatCurrency } = useCountry();
+    const [showDetails, setShowDetails] = useState(false);
+    const [selectedOrder, setSelectedOrder] = useState(null);
+    const [orderProducts, setOrderProducts] = useState([]);
 
     useEffect(() => {
         if (!userData.id) return;
@@ -31,60 +37,165 @@ const YourOrders = () => {
         console.log(orders);
     }, [orders]);
 
+    useEffect(() => {
+        if (!showDetails || !selectedOrder) {
+            setOrderProducts([]);
+            return;
+        }
+
+        // obtener información e imágenes de todos los productos
+        selectedOrder.detalles.forEach(detail => {
+            getProductById(detail.producto_id).then(product => {
+                const addDetail = {
+                    cantidad: detail.cantidad,
+                    precio: detail.precio,
+                    ...product,
+                };
+
+                setOrderProducts(orderProducts => [
+                    ...orderProducts,
+                    addDetail,
+                ]);
+            });
+        });
+    }, [showDetails, selectedOrder]);
+
     return (
         <DashBoard>
-            <section className="your-orders user-dashboard-container">
-                <div className="your-orders-types">
-                    {orderTypes.map(type => (
-                        <p
-                            key={type}
-                            onClick={() => setSelectedOrderType(type)}
-                            className={`your-orders-type ${type === selectedOrderType ? 'your-orders-type-active' : ''}`}
-                        >
-                            {type}
-                        </p>
-                    ))}
-                </div>
-
-                <div className="your-orders-container">
-                    {orders.map(order => (
-                        <div key={order} className="your-orders-item">
-                            <p>Fecha:</p>
-                            <p>{order.fecha_pedido}</p>
-
-                            <p>Total:</p>
-                            <p>{formatCurrency(order.total)}</p>
-
-                            <p>Estado:</p>
-                            <p>{order.estado}</p>
-
-                            <p>Cupon:</p>
-                            <p>
-                                {order.cupon !== null
-                                    ? 'No se uso cupón'
-                                    : 'Se uso cupón de descuento'}
+            {!showDetails && (
+                <div className="your-orders user-dashboard-container">
+                    <section className="your-orders-types">
+                        {orderTypes.map(type => (
+                            <p
+                                key={type}
+                                onClick={() => setSelectedOrderType(type)}
+                                className={`your-orders-type ${type === selectedOrderType ? 'your-orders-type-active' : ''}`}
+                            >
+                                {type}
                             </p>
+                        ))}
+                    </section>
 
-                            <h4 className="your-orders-address-title">
-                                Datos de Envío
-                            </h4>
+                    <section className="your-orders-container">
+                        {orders.map(order => (
+                            <div key={order.id} className="your-orders-item">
+                                <p>Fecha:</p>
+                                <p>{order.fecha_pedido}</p>
 
-                            <div className="your-orders-address">
-                                <AddressComponent address={order.envio} />
+                                <p>Total:</p>
+                                <p>{formatCurrency(order.total)}</p>
+
+                                <p>Estado:</p>
+                                <p>{order.estado}</p>
+
+                                <p>Cupon:</p>
+                                <p>
+                                    {order.cupon !== null
+                                        ? 'No se uso cupón'
+                                        : 'Se uso cupón de descuento'}
+                                </p>
+
+                                <h4 className="your-orders-address-title">
+                                    Datos de Envío
+                                </h4>
+
+                                <div className="your-orders-address">
+                                    <AddressComponent address={order.envio} />
+                                </div>
+
+                                <button
+                                    onClick={() => {
+                                        setShowDetails(true);
+                                        setSelectedOrder(order);
+                                    }}
+                                >
+                                    Ver detalles
+                                </button>
                             </div>
+                        ))}
 
-                            <button>Ver detalles</button>
-                        </div>
-                    ))}
-
-                    {orders.length === 0 && (
-                        <div className="dashboard-empty">
-                            <BsBagX size={128} />
-                            <p>No tienes pedidos</p>
-                        </div>
-                    )}
+                        {orders.length === 0 && (
+                            <div className="dashboard-empty">
+                                <BsBagX size={128} />
+                                <p>No tienes pedidos</p>
+                            </div>
+                        )}
+                    </section>
                 </div>
-            </section>
+            )}
+
+            {showDetails && (
+                <section className="your-orders-item">
+                    <p>Fecha:</p>
+                    <p>{selectedOrder.fecha_pedido}</p>
+
+                    <p>Total:</p>
+                    <p>{formatCurrency(selectedOrder.total)}</p>
+
+                    <p>Estado:</p>
+                    <p>{selectedOrder.estado}</p>
+
+                    <p>Cupon:</p>
+                    <p>
+                        {selectedOrder.cupon !== null
+                            ? 'No se uso cupón'
+                            : 'Se uso cupón de descuento'}
+                    </p>
+
+                    <h4 className="your-orders-address-title">
+                        Datos de Envío
+                    </h4>
+
+                    <div className="your-orders-address">
+                        <AddressComponent address={selectedOrder.envio} />
+                    </div>
+
+                    <h4 className="your-orders-address-title">
+                        Productos del pedido
+                    </h4>
+
+                    <div className="your-orders-products">
+                        {orderProducts.map(product => (
+                            <div
+                                key={product.id}
+                                className="your-orders-product-item"
+                            >
+                                <Slider>
+                                    {product.imagenes.map((image, index) => (
+                                        <img
+                                            key={image + index}
+                                            src={`/images/${image}`}
+                                            alt={product.descripcion}
+                                        />
+                                    ))}
+                                </Slider>
+
+                                <p>
+                                    Cantidad: {product.cantidad} Precio Normal:{' '}
+                                    {formatCurrency(product.precio)}
+                                </p>
+                                <p>
+                                    <strong>Descripción:</strong>{' '}
+                                    {product.descripcion}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+
+                    {selectedOrder.estado === 'procesando' && (
+                        <button>Cancelar pedido</button>
+                    )}
+
+                    <button
+                        onClick={() => {
+                            setShowDetails(false);
+                            setSelectedOrder(null);
+                        }}
+                    >
+                        Volver
+                    </button>
+                </section>
+            )}
         </DashBoard>
     );
 };
